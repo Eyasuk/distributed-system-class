a90e6a696bd08dd371e74d35670b039b1695752b
Eyasuk <eyasukibru290@gmail.com>
2022-12-12 10:08:28 +0300

    small bug fix

--- assignment/client.js
+++ assignment/client.js
@@ -2,8 +2,9 @@ import net from "net"
 
 const port = 1277;
 const host = "127.0.0.1"
-const client = new net.Socket()
+
 try {
+    const client = new net.Socket()
     client.connect({ port: port, host: host }, () => {
         console.log('Client is alive and establishing socket connection. \n');
     })
