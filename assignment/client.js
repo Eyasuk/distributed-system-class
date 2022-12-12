084cc2087d6cf4ef7a8a7f139de1ec7c68d48610
Eyasuk <eyasukibru290@gmail.com>
2022-12-12 10:07:04 +0300

    finshed client.js

--- assignment/client.js
+++ assignment/client.js
@@ -3,7 +3,13 @@ import net from "net"
 const port = 1277;
 const host = "127.0.0.1"
 const client = new net.Socket()
-client.connect({ port: port, host: host })
-client.on("data", (data) => {
-  console.log(data);
-})
+try {
+    client.connect({ port: port, host: host }, () => {
+        console.log('Client is alive and establishing socket connection. \n');
+    })
+    client.on("data", (data) => {
+        console.log(data.toString());
+    });
+} catch (error) {
+    console.log('Error establishing connection')
+}
