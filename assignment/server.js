de8db54c5f4a73fef2a99a42bab98a7260cd1c69
Eyasuk <eyasukibru290@gmail.com>
2022-12-12 09:58:19 +0300

    finshed server

--- assignment/server.js
+++ assignment/server.js
@@ -1,10 +1,24 @@
 import net from 'net';
 
+const host = "127.0.0.1";
 const port = 1277;
+const SIM_LENGTH = 10;
 
-const server = net.createServer((socket) => {
-  socket.end('data 1');
-})
+try {
+    const server = net.createServer((socket) => {
 
-server.listen(port);
+        for (let count = 0; count <= SIM_LENGTH; count++) {
+            socket.write(`connect_socket ${count} \n`)
+            console.log(`Server has written ${count} to socket \n`);
 
+        }
+        socket.end();
+
+
+    });
+    server.listen(port, () => {
+        console.log("Server is alive and wating for socket connection from client Information.\n");
+    });
+} catch (error) {
+    console.error('Socket error')
+}
