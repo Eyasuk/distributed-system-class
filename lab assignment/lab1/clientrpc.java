cc834dfc5a1490ec6ecc5484daa5adc57e93f365
Eyasu kibru <53485615+Eyasuk@users.noreply.github.com>
2023-01-16 00:33:22 +0300

    lab1

---
+++ lab assignment/lab1/clientrpc.java
@@ -0,0 +1,24 @@
+import java.rmi.*;
+import java.io.*;
+import java.rmi.server.*;
+public class clientrpc
+{
+public static void main(String arg[])
+{
+try
+{
+String serverurl="rmi://localhost/serverrpc";
+ucet ob=(ucet) Naming.lookup(serverurl);
+int r=ob.function(10,5);
+System.out.println("the answer of(a+b)^2 is:"+r);
+int t =ob.power(10,5);
+System.out.println("the answer of(a)^(b) is:"+t);
+double d=ob.log(10);
+System.out.println("the log value of the given number "+10+" is :"+d);
+}
+catch(Exception e)
+{
+System.out.println("error.."+e.getMessage());
+}
+}
+}
